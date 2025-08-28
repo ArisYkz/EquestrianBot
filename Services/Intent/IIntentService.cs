fab11effbed1d0bf2395bcb946a3588e46827592
ArisYkz <arisy4511@gmail.com>
2025-08-29 04:28:27 +0800

    Delete Services directory

--- Services/Intent/IIntentService.cs
+++
@@ -1,7 +0,0 @@
-namespace EquestrianBot.Api.Services.Intent;
-
-public interface IIntentService
-{
-    // Return null if no match; otherwise the baseline answer string.
-    string? TryAnswer(string query);
-}
